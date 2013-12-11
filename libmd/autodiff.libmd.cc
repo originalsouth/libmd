7f1fde7b6e2c8598f733e47349a747f46e6656f7
Benny <zuiden@ilorentz.org>
2013-12-11 12:08:56 +0100

    Implemented arcsin, arccos, arcctan in autodiff

--- libmd/autodiff.libmd.cc
+++ libmd/autodiff.libmd.cc
@@ -1,71 +1,157 @@
 // Single differentiation
-
 struct var
-{	ldf val, x;
+{
+    ldf val, x;
 	var() {}
-	var (ldf f, ldf fx = 1) : val(f), x(fx) {}
+    var (ldf f, ldf fx = 1.0) : val(f), x(fx) {}
 	ldf diff() { return x; }
 };
 
-var operator+ (const var& F, const var& G) { return var(F.val+G.val, F.x+G.x); }
-var operator- (const var& F, const var& G) { return var(F.val-G.val, F.x-G.x); }
-var operator- (const var& F) { return var(-F.val, -F.x); }
-var operator* (const var& F, const var& G) { return var(F.val*G.val, F.x*G.val + F.val*G.x); }
-var operator/ (const var& F, const var& G) { return var(F.val/G.val, (F.x*G.val - F.val*G.x) / pow(G.val,2)); }
+var operator+ (const var& F, const var& G)
+{
+    return var(F.val+G.val, F.x+G.x);
+}
 
-template<class number> var operator+ (const var& F, number a) { return var(F.val+a, F.x); }
-template<class number> var operator- (const var& F, number a) { return var(F.val-a, F.x); }
-template<class number> var operator* (const var& F, number a) { return var(F.val*a, F.x*a); }
-template<class number> var operator/ (const var& F, number a) { return var(F.val/a, F.x/a); }
+var operator- (const var& F, const var& G)
+{
+    return var(F.val-G.val, F.x-G.x);
+}
 
-template<class number> var operator+ (number a, const var& F) { return F+a; }
-template<class number> var operator- (number a, const var& F) { return (-F)+a; }
-template<class number> var operator* (number a, const var& F) { return F*a; }
+var operator- (const var& F)
+{
+    return var(-F.val, -F.x);
+}
+
+var operator* (const var& F, const var& G)
+{
+    return var(F.val*G.val, F.x*G.val + F.val*G.x);
+}
+
+var operator/ (const var& F, const var& G)
+{
+    return var(F.val/G.val, (F.x*G.val - F.val*G.x) / pow(G.val,2));
+}
+
+template<class number> var operator+ (const var& F, number a)
+{
+    return var(F.val+a, F.x);
+}
+
+template<class number> var operator- (const var& F, number a)
+{
+    return var(F.val-a, F.x);
+}
+
+template<class number> var operator* (const var& F, number a)
+{
+    return var(F.val*a, F.x*a);
+}
+
+template<class number> var operator/ (const var& F, number a)
+{
+    return var(F.val/a, F.x/a);
+}
+
+template<class number> var operator+ (number a, const var& F)
+{
+    return F+a;
+}
+
+template<class number> var operator- (number a, const var& F)
+{
+    return (-F)+a;
+}
+
+template<class number> var operator* (number a, const var& F)
+{
+    return F*a;
+}
+
 template<class number> var operator/ (number a, const var& F)
-{ return var(a/F.val, -a*F.x/pow(F.val,2));
+{
+    return var(a/F.val, -a*F.x/pow(F.val,2));
 }
 
 var sqrt (const var& F)
-{ return var(sqrt(F.val), F.x / 2 / sqrt(F.val));
+{
+    return var(sqrt(F.val), F.x / 2.0 / sqrt(F.val));
 }
+
 template<class number> var pow (const var& F, number n)
-{ return var(pow(F.val, n), F.x * n * pow(F.val, n-1));
+{
+    return var(pow(F.val, n), F.x * n * pow(F.val, n-1));
 }
+
 var exp (const var& F)
-{ return var(exp(F.val), F.x * exp(F.val));
+{
+    return var(exp(F.val), F.x * exp(F.val));
 }
 template<class number> var pow (number a, const var& G)
-{ return var(pow(a, G.val), G.x * log(a) * pow(a, G.val));
+{
+    return var(pow(a, G.val), G.x * log(a) * pow(a, G.val));
 }
 var pow (const var& F, const var& G)
-{ return var(pow(F.val, G.val), (G.x*log(F.val) + F.x*G.val/F.val) * pow(F.val, G.val));
+{
+    return var(pow(F.val, G.val), (G.x*log(F.val) + F.x*G.val/F.val) * pow(F.val, G.val));
 }
+
 var log (const var& F)
-{ return var(log(F.val), F.x / F.val);
+{
+    return var(log(F.val), F.x / F.val);
 }
+
 var sin (const var& F)
-{ return var(sin(F.val), F.x * cos(F.val));
+{
+    return var(sin(F.val), F.x * cos(F.val));
 }
+
 var cos (const var& F)
-{ return var(cos(F.val), -F.x * sin(F.val));
+{
+    return var(cos(F.val), -F.x * sin(F.val));
 }
-var tan (const var& F) { return sin(F)/cos(F); }
+
+var tan (const var& F)
+{
+    return sin(F)/cos(F);
+}
+
 var sinh (const var& F)
-{ return var(sinh(F.val), F.x * cosh(F.val));
+{
+    return var(sinh(F.val), F.x * cosh(F.val));
 }
+
 var cosh (const var& F)
-{ return var(cosh(F.val), F.x * sinh(F.val));
+{
+    return var(cosh(F.val), F.x * sinh(F.val));
 }
-var tanh (const var& F) { return sinh(F)/cosh(F); }
 
+var tanh (const var& F)
+{
+    return sinh(F)/cosh(F);
+}
 
+var asin (const var& F)
+{
+    return var(asin(F.val),F.x/sqrt(1.0-pow(F.val,2)));
+}
+
+var acos (const var& F)
+{
+    return var(acos(F.val),-F.x/sqrt(1.0-pow(F.val,2)));
+}
+
+var atan (const var& F)
+{
+    return var(atan(F.val),F.x/(pow(F.val,2)+1.0));
+}
 
 // Double differentiation
 
 struct var2
-{	ldf val, x, y, xy;
+{
+    ldf val, x, y, xy;
 	var2() {}
-	var2 (ldf f, ldf fx = 1, ldf fy = 1, ldf fxy = 0) : val(f), x(fx), y(fy), xy(fxy) {}
+    var2 (ldf f, ldf fx = 1.0, ldf fy = 1.0, ldf fxy = 0.0) : val(f), x(fx), y(fy), xy(fxy) {}
 	ldf diff1() { return x; }
 	ldf diff2() { return y; }
 	ldf diff12() { return xy; }
@@ -179,11 +265,13 @@ typedef var (*potentialpointer)(var,var,vector<ldf> *);
 // Example functions
 
 template<ui dim, class number> number function1 (number X[])
-{	return pow(X[0],3) * exp(-X[1]) * cos(X[dim-1]);
+{
+    return pow(X[0],3) * exp(-X[1]) * cos(X[dim-1]);
 }
 
 template<ui dim, class number> number function2 (number X[])
-{	return X[0]*X[0]/X[1] + X[1] + 7;
+{
+    return X[0]*X[0]/X[1] + X[1] + 7;
 }
 
 template<class number> number LJ (number r, number rsq, vector<ldf> *parameters)
@@ -202,24 +290,25 @@ template<class number> number LJ2 (number r, number rsq, vector<ldf> *parameters
     return 4.0*e*(pow(s,12)/pow(r,12)-pow(s,6)/pow(r,6));;
 }
 
-
-
 // Example usage
 
 template<ui dim> ldf df (functionpointer func, ui i, ldf X[])
-{	var Y[dim];
+{
+    var Y[dim];
 	for (ui d = 0; d < dim; d++)
 		Y[d] = var(X[d], i==d);
 	return func(Y).diff();
 }
 
 ldf dr (potentialpointer func, ldf r, ldf rsq, vector<ldf> *parameters)
-{	var R(r,1), Rsq(rsq, 2*r);
+{
+    var R(r,1), Rsq(rsq, 2*r);
 	return func(R, Rsq, parameters).diff();
 }
 
 template<ui dim> ldf d2f (functionpointer2 func, ui i, ui j, ldf X[])
-{	var2 Y[dim];
+{
+    var2 Y[dim];
 	for (ui d = 0; d < dim; d++)
 		Y[d] = var2(X[d], i==d, j==d, 0);
 	return func(Y).diff12();
@@ -228,7 +317,8 @@ template<ui dim> ldf d2f (functionpointer2 func, ui i, ui j, ldf X[])
 
 
 int test()
-{	ldf X[] = {1,2,3};
+{
+    ldf X[] = {1,2,3};
 	ldf Y[] = {5,7};
 	printf("%Lf %Lf  %Lf %Lf %Lf\n", df<3>(&function1<3>,0,X), df<3>(&function1<3>,2,X), d2f<3>(&function1<3>,0,0,X), d2f<3>(&function1<3>,0,1,X), d2f<3>(&function1<3>,1,1,X));
 	printf("%Lf %Lf  %Lf %Lf %Lf\n", df<2>(&function2<2>,0,Y), df<2>(&function2<2>,1,Y), d2f<2>(&function2<2>,0,0,Y), d2f<2>(&function2<2>,0,1,Y), d2f<2>(&function2<2>,1,1,Y));
