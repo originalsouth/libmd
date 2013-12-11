b52e83b95888c3d64b6ccf544d4d73a081a6a930
Jayson Paulose <paulose@verster.lorentz.leidenuniv.nl>
2013-12-11 14:43:49 +0100

    working: read points and springs from input files of type supported by stephan's code

---
+++ tests/randomspring/randomspring.cc
@@ -0,0 +1,72 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Simple test file                                                                                             //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "../../libmd.cc"
+#include "../BaX/BaX.h"
+#include "springio.cc"
+
+using namespace std;
+
+
+//~ string ptfile = "triangle.pts";
+//~ string bfile = "triangle.bds";
+string ptfile = "smd.pts";
+string bfile = "smd.bds";
+
+int main()
+{   
+    unsigned int W=500,H=500;
+    bitmap bmp(W,H);
+    color pix[]={RED,GREEN,BLUE};
+    bmp.fillup(BLACK);
+    
+    // initialize points
+    // need correct-sized arrays
+    ui systemsize = number_of_lines(ptfile);
+    cout << systemsize << endl;
+    ldf x[systemsize];
+    ldf y[systemsize];
+    
+    read_points_ulrich(ptfile,x,y);
+    
+    // make md system
+    md<2> sys(systemsize);
+    sys.parallel.set(1);
+    sys.network.rcosq=400.0;
+    sys.network.rco=20.0;
+    sys.network.sszsq=500.0;
+    sys.simbox.L[0]=50.5; // make a larger box to force relaxation. sytem size for no forces: 45.5
+    sys.simbox.L[1]=50.5;
+    sys.simbox.bcond[0]=1;
+    sys.simbox.bcond[1]=1;
+    sys.integrator.method=1;
+    sys.import_pos(&x,&y);
+
+
+    // worst case: each particle a unique type, each bond a unique interaction
+    for (int i = 0; i < systemsize; i++) sys.particles[i].type = i;
+    
+    // initialize bonds
+    read_bonds_ulrich(bfile,sys);
+
+    // seg faults with thomas's cell method. why?
+    sys.indexdata.method = 1;
+    sys.index();
+    sys.network.update=false;
+
+    for(ui h=0;h<100;h++)
+    {
+        for(ui i=0;i<systemsize;i++) bmp.set(W*sys.particles[i].x[0]/sys.simbox.L[0]+W/2.0,H*sys.particles[i].x[1]/sys.simbox.L[1]+H/2,GREEN);
+        bmp.save_png_seq(const_cast<char *>("sim"));
+        sys.timesteps(100);
+        cout << h << endl;
+    }
+    for(ui i=0;i<systemsize;i++) bmp.set(W*sys.particles[i].x[0]/sys.simbox.L[0]+W/2.0,H*sys.particles[i].x[1]/sys.simbox.L[1]+H/2,BLUE);
+    bmp.save_png_seq(const_cast<char *>("sim"));
+    return EXIT_SUCCESS;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Simple test file                                                                                             //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ tests/randomspring/springio.cc
@@ -0,0 +1,43 @@
+////////////////////////////////////
+// I/O for 2D spring networks        //
+////////////////////////////////////
+
+using namespace std;
+
+ui number_of_lines(string ptfile) {
+    int nl = 0;
+    string line;
+    ifstream myfile(ptfile);
+
+    while (getline(myfile, line))
+        ++nl;
+    return nl;
+}
+
+void read_points_ulrich(string ptfile, ldf *x, ldf* y) {
+    FILE* inputM;
+    double xin, yin;
+    
+    vector<double> xv(0);
+    vector<double> yv(0);
+    inputM = fopen(ptfile.c_str(), "r");
+    while (!(feof(inputM))) {
+        fscanf(inputM, "%lf %lf\n", &xin, &yin);
+        xv.push_back(xin); yv.push_back(yin);
+    }
+    
+    copy(xv.begin(), xv.end(), x);
+    copy(yv.begin(), yv.end(), y);
+}
+
+template<ui dim> void read_bonds_ulrich(string bfile, md<dim> &sys) {
+    ui p1in, p2in, dummy;
+    ldf kin, l0in;
+    FILE* inputM = fopen(bfile.c_str(), "r");
+    while (!(feof(inputM))) {
+        fscanf(inputM, "%d %d %d %Lf %Lf\n", &p1in, &p2in, &dummy, &kin, &l0in);
+        // spring with k and r0
+        vector<ldf> a={kin,l0in};
+        sys.add_typeinteraction(p1in, p2in, 2, &a);
+    }
+}
