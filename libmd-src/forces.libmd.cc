88217f68afa70eb768e3adfeb5dc7ce7fc8b2848
Jayson Paulose <paulose@ilorentz.org>
2014-03-24 18:44:41 +0100

    Minor reformat

--- libmd-src/forces.libmd.cc
+++ libmd-src/forces.libmd.cc
@@ -14,7 +14,5 @@ template<ui dim> void DAMPING(ui i,vector<ui> *particles,vector<ldf> *parameters
 template<ui dim> void DISSIPATION(ui i,vector<ui> *particles,vector<ldf> *parameters,void *sys)
 {
     ldf b=parameters->at(0);
-    for(auto it=particles->begin();it!=particles->end();it++) for(ui d=0;d<dim;d++) {
-        SYS->particles[i].F[d]+=b*(SYS->dv(d,i,*it));
-    }
+    for(auto it=particles->begin();it!=particles->end();it++) for(ui d=0;d<dim;d++) SYS->particles[i].F[d]+=b*(SYS->dv(d,i,*it));
 }
