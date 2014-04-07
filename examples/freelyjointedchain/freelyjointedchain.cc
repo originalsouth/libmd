f6902c905e87bdd3b383f9d5277e83c4129aa45d
Thomas Beuman <beuman@lorentz.leidenuniv.nl>
2014-04-07 15:45:23 +0200

    Modified skinner to use type_interaction when no sp_interaction is found -- modified freelyjointedchain accordingly

--- examples/freelyjointedchain/freelyjointedchain.cc
+++ examples/freelyjointedchain/freelyjointedchain.cc
@@ -36,10 +36,10 @@ int main()
     vector<ldf> b={10.0,sys.simbox.L[1]/((ldf)M)};
     sys.add_typeinteraction(0,0,POT::YUKAWA,&a);
     sys.add_typeinteraction(0,1,POT::YUKAWA,&a);
-    sys.add_typeinteraction(1,1,POT::HOOKEAN,&b);
+    ui hooke=sys.add_interaction(POT::HOOKEAN,&b);
     sys.add_sptype();
     for(ui i=N;i<N+M;i++) sys.sp_ingest(0,0,i);
-    for(ui i=N;i<N+M;i++) sys.add_sp_interaction(0,i-N,i-N+1,2);
+    for(ui i=N;i<N+M;i++) sys.add_sp_interaction(0,i-N,i-N+1,hooke);
     sys.export_pos(x,y);
     bmp.fillup(BLACK);
     for(ui i=0;i<N;i++) bmp.solidkykel(2.0,W*x[i]/sys.simbox.L[0]+W/2.0,H*y[i]/sys.simbox.L[1]+H/2,pix[0]);
