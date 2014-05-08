f3aa2b719de78916cd0b953d37aa1b3ab7c3c7b7
Thomas Beuman <beuman@lorentz.leidenuniv.nl>
2014-05-08 18:10:22 +0200

    Fixed one-liner fail

--- tests/regressiontester/regressiontester.cc
+++ tests/regressiontester/regressiontester.cc
@@ -118,7 +118,7 @@ struct testunit
                 break;
                 case 2: p=test_modify_sp_bonds();
                 break;
-                case 3: p=test_remove_sp();
+                case 3: p=test_remove_particles();
                 break;
                 default: printf("test_not_found(%d,%d): " IO_BOLDRED "failed" IO_RESET ".\n",i,j); return;
             }
