f81aac244232afe778406ee16cb89ab7294e69ab
Benny van Zuiden <zuiden@lorentz.leidenuniv.nl>
2015-04-21 12:15:42 +0200

    Fixed initialization for pbcond

--- libmd-src/box.libmd.cc
+++ libmd-src/box.libmd.cc
@@ -5,12 +5,12 @@
 const ldf mxinv_eps=sqrt(numeric_limits<ldf>::epsilon()); // matrix inversion cutoff
 
 template<ui dim> box<dim>::box()
-{   
-    //! 
-    //! Constructer for the system box. Assigns memory to the 
+{
+    //!
+    //! Constructer for the system box. Assigns memory to the
     //! arrays that store the box size, boundary conditions, and shear
     //! conditions, all of which depend on the dimension \c dim.
-    //! 
+    //!
     memset(bcond,0,dim*sizeof(uc));
     for(ui d=0;d<dim;d++) L[d]=10.0;
     memset(vshear,0,dim*dim*sizeof(ldf));
@@ -18,14 +18,15 @@ template<ui dim> box<dim>::box()
     memset(LshearInv,0,dim*dim*sizeof(ldf));
     for(ui d=0;d<dim;d++) Lshear[d][d]=L[d],LshearInv[d][d]=1.0/L[d];
     useLshear=false;
+    pbc=false;
 }
 
 template<ui dim> void box<dim>::shear_boundary(ui i,ui j,ldf velocity)
-{   
-    //! 
+{
+    //!
     //! Apply a dynamic shear to  the boundary normal to spatial dimension \c j by shear speed
     //! \c velocity in direction \c i. Requires \f$i\neq j\f$.
-    //! 
+    //!
     if(i==j)
     {
         ERROR("shear velocity must be perpendicular to boundary");
@@ -38,13 +39,13 @@ template<ui dim> void box<dim>::shear_boundary(ui i,ui j,ldf velocity)
 }
 
 template<ui dim> void box<dim>::skew_boundary(ui i, ui j, ldf displacement)
-{   
-    //! 
+{
+    //!
     //! Apply a static shear by displacing the boundary normal to spatial
-    //! dimension \c j in direction \c i by an amount \c displacement. 
-    //! Requires \f$i\neq j\f$. The boundary conditions in dimension j 
-    //! must be set to BOXSHEAR (set by default) or HARD; particle dynamics do 
-    //! not respect the shear if PERIODIC is used. 
+    //! dimension \c j in direction \c i by an amount \c displacement.
+    //! Requires \f$i\neq j\f$. The boundary conditions in dimension j
+    //! must be set to BOXSHEAR (set by default) or HARD; particle dynamics do
+    //! not respect the shear if PERIODIC is used.
     //!
     if(i==j)
     {
@@ -61,15 +62,15 @@ template<ui dim> void box<dim>::skew_boundary(ui i, ui j, ldf displacement)
 
 
 template<ui dim> ldf det (ldf Ain[dim][dim], ldf B[dim][dim])
-{   
-    //! 
+{
+    //!
     //! Return the determinant of the array \c Ain, and store its
     //! inverse in the array \c B if the determinant is nonzero.
-    //! 
+    //!
     ui i, j, k;
     int sgn = 1;
     ldf d = 1, t1, t2;
-  
+
     // clone Ain so that it is unchanged
     ldf A[dim][dim];
     copy(&Ain[0][0], &Ain[0][0]+dim*dim,&A[0][0]);
@@ -114,11 +115,11 @@ template<ui dim> ldf det (ldf Ain[dim][dim], ldf B[dim][dim])
 
 
 template<ui dim> void box<dim>::invert_box()
-{   
-    //! 
+{
+    //!
     //! Invert the system box \c matrix box<dim>::Lshear and store the result
-    //! in \c box<dim>::LshearInv. 
-    //! 
+    //! in \c box<dim>::LshearInv.
+    //!
     ldf d = det(Lshear, LshearInv);
     if (fabs(d) < mxinv_eps) {
         ERROR("singular matrix encountered during box matrix inversion");
