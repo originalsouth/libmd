originalsouth/libmd

commit 4ca993cfc708e827da57d18d743d5296b61e9e6a

Pathed issue75 in branch issue75

libmd-src/md/setget.md.libmd.cc
@@ -11,7 +11,7 @@ template<ui dim> void md<dim>::set_damping(ldf coefficient)
     {
         DEBUG_2("activating damping with damping coefficient: " F_LDF,coefficient);
         vector<ldf> parameters(1,coefficient);
-        avars.noftypedamping=add_forcetype(EXTFORCE::DAMPING,nullptr,&parameters);
+        avars.noftypedamping=add_forcetype(EXTFORCE::DAMPING,nullptr,parameters);
         assign_all_forcetype(avars.noftypedamping);
     }
     else
@@ -57,7 +57,7 @@ template<ui dim> void md<dim>::set_rco(ldf rco)
     //! Sets <tt>network.rco</tt>, the interaction cut-off distance, to <tt>rco</tt>.
     //!
     network.rco=rco;
-    for (auto itype: network.library) itype.vco = v(itype.potential,network.rco,&itype.parameters);
+    for (auto itype: network.library) itype.vco = v(itype.potential,network.rco,itype.parameters);
     if (network.rco > network.ssz)
     {
         WARNING("network.rco is now larger than network.ssz (" F_LDF " > " F_LDF ")",network.rco,network.ssz);
@@ -71,7 +71,7 @@ template<ui dim> void md<dim>::set_rco(ui interaction,ldf rco)
     //!
     auto itype=network.library[interaction];
     itype.rco = rco;
-    itype.vco = v(itype.potential,rco,&itype.parameters);
+    itype.vco = v(itype.potential,rco,itype.parameters);
     if (rco > network.ssz)
     {
         WARNING("this rco is now larger than network.ssz (" F_LDF " > " F_LDF ")",rco,network.ssz);
@@ -87,7 +87,7 @@ template<ui dim> void md<dim>::set_ssz(ldf ssz)
     set_reserve(ssz);
     for(auto itype:network.library) if(itype.rco>network.ssz)
     {
-        WARNING("rco of interaction %zu is now larger than network.ssz (" F_LDF " > " F_LDF ")",&itype-&network.library[0],itype.rco,network.ssz);
+        WARNING("rco of interaction %zu is now larger than network.ssz (" F_LDF " > " F_LDF ")",itype-&network.library[0],itype.rco,network.ssz);
     }
     if(network.rco>network.ssz)
     {

examples/boxshear/shearxy.cc
@@ -42,7 +42,7 @@ int main()
     sys.simbox.bcond[0]=BCOND::PERIODIC;
     sys.simbox.bcond[1]=BCOND::PERIODIC;
     vector<ldf> a={1.0,1.0};
-    sys.add_typeinteraction(0,0,2,&a);
+    sys.add_typeinteraction(0,0,2,a);
     sys.import_pos(x,y);
     sys.import_vel(dx,dy);
     sys.index();

examples/curved-orbit/curved-orbit.cc
@@ -32,7 +32,7 @@ int main()
     sys.import_vel(dx,dy);
     sys.history();
     vector<ldf> a={-1.0};
-    sys.add_typeinteraction(0,0,POT::COULOMB,&a);
+    sys.add_typeinteraction(0,0,POT::COULOMB,a);
     sys.index();
     sys.network.update=false;
     FILE *energy;

examples/dimers/dimers.cc
@@ -34,8 +34,8 @@ int main()
     sys.set_ssz(2.0);
     vector<ldf> a={1.0,100.0};
     vector<ldf> b={1000.0,sys.simbox.L[1]/160.0};
-    sys.add_interaction(POT::YUKAWA,&a);
-    sys.add_interaction(POT::HOOKEAN,&b);
+    sys.add_interaction(POT::YUKAWA,a);
+    sys.add_interaction(POT::HOOKEAN,b);
     sys.add_typeinteraction(0,0,0);
     sys.add_typeinteraction(0,1,1);
     sys.add_typeinteraction(1,1,0);

examples/freelyjointedchain/freelyjointedchain.cc
@@ -34,9 +34,9 @@ int main()
     sys.import_vel(dx,dy);
     vector<ldf> a={1.0,100.0};
     vector<ldf> b={10.0,sys.simbox.L[1]/((ldf)M)};
-    sys.add_typeinteraction(0,0,POT::YUKAWA,&a);
-    sys.add_typeinteraction(0,1,POT::YUKAWA,&a);
-    ui hooke=sys.add_interaction(POT::HOOKEAN,&b);
+    sys.add_typeinteraction(0,0,POT::YUKAWA,a);
+    sys.add_typeinteraction(0,1,POT::YUKAWA,a);
+    ui hooke=sys.add_interaction(POT::HOOKEAN,b);
     sys.add_sp(sys.add_sptype());
     for(ui i=N;i<N+M;i++) sys.sp_ingest(0,i);
     for(ui i=N;i<N+M;i++) sys.add_sp_interaction(0,i-N,i-N+1,hooke);

examples/freelyjointedchain/freelyjointedchain2.cc
@@ -33,9 +33,9 @@ int main()
     sys.import_pos(x,y);
     vector<ldf> a={1.0,100.0};
     vector<ldf> b={100.0,sys.simbox.L[1]/100.0};
-    sys.add_typeinteraction(0,0,POT::YUKAWA,&a);
-    sys.add_typeinteraction(0,1,POT::YUKAWA,&a);
-    ui hooke=sys.add_interaction(POT::HOOKEAN,&b);
+    sys.add_typeinteraction(0,0,POT::YUKAWA,a);
+    sys.add_typeinteraction(0,1,POT::YUKAWA,a);
+    ui hooke=sys.add_interaction(POT::HOOKEAN,b);
     ldf spx[]={0.0,-sys.simbox.L[1]/2.0};
     sys.add_sp(sys.add_sptype());
     for(ui i=0;i<100;i++) sys.add_sp_interaction(0,i,i+1,hooke);

examples/hertzianpulse/hertzianpulse.cc
@@ -64,7 +64,7 @@ int main()
     sys.simbox.bcond[0]=BCOND::PERIODIC;
     sys.simbox.bcond[1]=BCOND::PERIODIC;
     vector<ldf> a={1.0,1.0,2.5}; // hertzian
-    sys.add_typeinteraction(0,0,POT::ANHARMONICSPRING,&a);
+    sys.add_typeinteraction(0,0,POT::ANHARMONICSPRING,a);
 
 
     sys.indexdata.method=INDEX::CELL;

examples/orbit/orbit.cc
@@ -31,7 +31,7 @@ int main()
     sys.import_pos(x,y);
     sys.import_vel(dx,dy);
     vector<ldf> a={-1.0};
-    sys.add_typeinteraction(0,0,POT::COULOMB,&a);
+    sys.add_typeinteraction(0,0,POT::COULOMB,a);
     sys.index();
     sys.network.update=false;
     FILE *energy;

commit 9942f4b3478562a3601ea22651b9bfb403fb1add

Fixed regression introduced in 4ca993cfc708e827da57d18d743d5296b61e9e6a

libmd-src/md/setget.md.libmd.cc
@@ -87,7 +87,7 @@ template<ui dim> void md<dim>::set_ssz(ldf ssz)
     set_reserve(ssz);
     for(auto itype:network.library) if(itype.rco>network.ssz)
     {
-        WARNING("rco of interaction %zu is now larger than network.ssz (" F_LDF " > " F_LDF ")",itype-&network.library[0],itype.rco,network.ssz);
+        WARNING("rco of interaction %zu is now larger than network.ssz (" F_LDF " > " F_LDF ")",&itype-&network.library[0],itype.rco,network.ssz);
     }
     if(network.rco>network.ssz)
     {